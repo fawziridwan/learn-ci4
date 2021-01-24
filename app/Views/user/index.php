9afaeac2aafae05304ed72a6e08c2f77e73de9db
fawziridwan14 <fawziridwan@gmail.com>
2021-01-24 18:25:55 +0700

    add index user

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -1,7 +1,32 @@
 <?= $this->extend('templates/index'); ?>
 <?= $this->section('page-content'); ?>
-    <div class="container-fluid">
-        <!-- Page Heading -->
-        <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">My Profile</h1>
+
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card-mb-3" style="max-width: 540px;">
+                <div class="row no-gutter">
+                    <div class="col-md-4">
+                        <img class="card-img-top" src="<?= base_url('/img/' . user()->user_image) ?>" alt="<?= user()->username ?>">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <h4><?= user()->username; ?></h4>
+                                </li>
+                                <?php if (user()->fullname) : ?>
+                                    <li class="list-group-item"><?= user()->fullname; ?></li>
+                                <?php endif; ?>
+                                <li class="list-group-item"><?= user()->email; ?></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 <?= $this->endSection(); ?>
